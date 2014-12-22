7605152505f6135907ba38f7c9041390139584c3
maiah <jjmmacariola@gmail.com>
2014-12-22 13:33:50 +0800

    Added support for if-else statement

--- express.go
+++ express.go
@@ -14,6 +14,8 @@ func main() {
 
 	// Compile Express to Go
 	boxCode = strings.Replace(boxCode, "fun()", "func() {", -1)
+	boxCode = strings.Replace(boxCode, "iend", ")", -1)
+	boxCode = strings.Replace(boxCode, "vend", ")", -1)
 	boxCode = strings.Replace(boxCode, "end", "}", -1)
 
 	reader := bufio.NewReader(bytes.NewBuffer([]byte(boxCode)))
@@ -25,13 +27,23 @@ func main() {
 		ss_s := string(ss)
 
 		if err == nil {
-			if strings.HasPrefix(ss_s, "interface ") {
+			if strings.TrimSpace(ss_s) == "import" {
+				ss_s = strings.Replace(ss_s, "import", "import (", 1)
+			} else if strings.TrimSpace(ss_s) == "var" {
+				ss_s = strings.Replace(ss_s, "var", "var (", 1)
+			} else if strings.HasPrefix(ss_s, "interface ") {
 				ss_plit := strings.Split(ss_s, " ")
 				ss_s = "type " + ss_plit[1] + " interface {"
 			} else if strings.HasPrefix(ss_s, "struct ") {
 				ss_s = "type " + strings.Split(ss_s, " ")[1] + " struct {"
 			} else if strings.HasPrefix(ss_s, "fun ") {
 				ss_s = strings.Replace(ss_s, "fun ", "func ", 1) + " {"
+			} else if strings.HasPrefix(strings.TrimSpace(ss_s), "if ") {
+				ss_s = ss_s + " {"
+			} else if strings.TrimSpace(ss_s) == "else" {
+				ss_s = "} " + ss_s + " {"
+			} else if strings.HasPrefix(strings.TrimSpace(ss_s), "else ") {
+				ss_s = "} " + ss_s + " {"
 			}
 
 			lines += ss_s + "\n"
